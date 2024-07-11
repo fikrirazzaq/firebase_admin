ea3d9e38e80de7f089e5b8ec8242adc6ab51a3b8
Fikri Razzaq Arrasyid <fikri.arrasyid@fnbees.com>
2024-07-11 10:47:43 +0800

    update firebase_dart ref.

--- lib/src/database.dart
+++ lib/src/database.dart
@@ -41,16 +41,13 @@ class Database implements FirebaseService {
 
   /// Do not call this constructor directly. Instead, use app().database.
   Database(this.app)
-      : _database = StandaloneFirebaseDatabase(
-            app.options.databaseUrl ??
-                'https://${app.projectId}.firebaseio.com/',
+      : _database = StandaloneFirebaseDatabase(app.options.databaseUrl ?? 'https://${app.projectId}.firebaseio.com/',
             authTokenProvider: _AuthTokenProvider(app.internals));
 
   /// Returns a [Reference] representing the location in the Database
   /// corresponding to the provided [path]. If no path is provided, the
   /// Reference will point to the root of the Database.
-  DatabaseReference ref([String? path]) =>
-      _database.reference().child(path ?? '');
+  DatabaseReference ref([String? path]) => _database.reference().child(path ?? '');
 
   @override
   Future<void> delete() async {
